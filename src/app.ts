4f6180c28fc9d42e05b8e4fbd6e2a80555b1fc17
user05 <user05@ALogDemo.local>
2023-12-08 17:09:05 +0700

    I added text to the README file

--- src/app.ts
+++ src/app.ts
@@ -11,8 +11,15 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.json({
         message: 'Hello this is Restful API Express + TypeScirpt + Prisma made by Apirak Kaewpachum !!',
+        contract:{
+            Email:'Netapirak@gmail.com',
+            Phone:'095-604-3539'
+        }
     })
 })
+
+app.listen(port, () => console.log(`Application is running on http://localhost:${port}/`))
+
 
 app.post('/create/user', async (req, res) => {
     try {
@@ -21,7 +28,6 @@ app.post('/create/user', async (req, res) => {
             data: {
                 name,
                 email,
-
             },
         })
         res.json(user);
@@ -33,7 +39,7 @@ app.post('/create/user', async (req, res) => {
     }
 })
 
-app.get('/findUsers', async (req, res) => {
+app.get('/users', async (req, res) => {
     try {
         const { name, email } = req.body
         const users = await prisma.user.findMany()
@@ -46,7 +52,7 @@ app.get('/findUsers', async (req, res) => {
     }
 })
 
-app.get('/find/:id', async (req, res) => {
+app.get('/finduser/:id', async (req, res) => {
     try {
         const { id } = req.params
         const user = await prisma.user.findMany({
@@ -80,7 +86,7 @@ app.get('/post/:id', async (req, res) => {
     }
 })
 
-app.get('/post/', async (req, res) => {
+app.get('/posts/', async (req, res) => {
     try {
         const post = await prisma.post.findMany()
         res.json(post);
@@ -93,16 +99,16 @@ app.get('/post/', async (req, res) => {
     }
 })
 
-app.post('/feed/creat', async (req, res) => {
+app.post('/post/create', async (req, res) => {
     try {
 
         const { authorId, title, content, published } = req.body
         const userFeed = await prisma.post.create({
-            data: {
+            data: { 
                 title,
                 content,
-                authorId,
-                published
+                authorId, 
+                published 
             },
         })
         res.json(userFeed);
@@ -115,9 +121,119 @@ app.post('/feed/creat', async (req, res) => {
     }
 })
 
+app.post('/post/create/:id', async (req, res) => {
+    try {
+        const { id } = req.params
+        const {  title, content, published } = req.body
+        const createdPost = await prisma.post.create({
+            data: {
+                title,
+                content,
+                published,
+                
+                author: {
+                    connect: {
+                        id: Number(id),
+                    },
+                },
+            },
+        })
+        res.json(createdPost);
+
+    } catch (error: any) {
+        console.log(error.message)
+        res.status(500).json({
+            message: "Internal Server Error",
+        })
+    }
+})
+
+app.get('/user/post/:id', async (req, res) => {
+    try {
+        const { id } = req.params
+
+        const posts = await prisma.user
+            .findUnique({
+                where: {
+                    id: Number(id),
+                },
+            })
+            .posts({
+                where: { published: true },
+            })
+
+        res.json(posts)
+    } catch (error: any) {
+        console.log(error.message)
+        res.status(500).json({
+            message: "Internal Server Error",
+        })
+    }
+})
+
+app.put('/edit/user/:id', async (req, res) => {
+    try {
+        const { id } = req.params
+        const {name , email} = req.body
+        const edituser = await prisma.user.update({
+            where:{
+                id: Number(id)
+            },
+            data:{ name, email}
+        })
+
+        res.json(edituser)
+    } catch (error: any) {
+        console.log(error.message)
+        res.status(500).json({
+            message: "Internal Server Error",
+        })
+    }
+})
 
 
 
-app.listen(port, () => console.log(`Application is running on http://localhost:${port}/`))
+app.put('/edit/post/:id', async (req, res) => {
+    try {
+        const { id } = req.params
+        const {title , content,published} = req.body
+        const editpost = await prisma.post.update({
+            where:{
+                id: Number(id)
+            },
+            data:{ title , content,published}
+        })
 
+        res.json(editpost)
+    } catch (error: any) {
+        console.log(error.message)
+        res.status(500).json({
+            message: "Internal Server Error",
+        })
+        res.json(error.message)
+    }
+})
 
+app.put('/edit/user/post/:id', async (req, res) => {
+    try {
+        const {  id } = req.params
+        const {authorId,title , content,published} = req.body
+        const posts = await prisma.post.update({
+            where:{
+                id: Number(id)
+            },
+            data:{ 
+                authorId,
+                title ,
+                 content,
+                 published}
+            })
+
+        res.json(posts)
+    } catch (error: any) {
+        console.log(error.message)
+        res.status(500).json({
+            message: "Internal Server Error",
+        })
+    }
+})
